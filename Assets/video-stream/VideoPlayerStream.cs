7dad4d38a61afe58ff9d1ce3d83e44738b9c6bf1
Jared Stokes <stokesjd@gmail.com>
2019-02-17 15:18:29 -0800

    video stream

---
+++ Assets/video-stream/VideoPlayerStream.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class VideoPlayerStream : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public RawImage rawImage;
+
+    void Start()
+    {
+        StartCoroutine(PlayVideo());
+    }
+
+    IEnumerator PlayVideo()
+    {
+        videoPlayer.Prepare();
+        while (!videoPlayer.isPrepared)
+        {
+            yield return null;
+            break;
+        }
+
+        videoPlayer.Play();
+    }
+
+    private void Update()
+    {
+        rawImage.texture = videoPlayer.texture;
+    }
+}
